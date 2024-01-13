e728f3b699c4754cabc60c9b308c7665377b7b46
hgandras <hgandras@gmail.com>
2024-01-13 16:57:07 +0100

    added echoing

--- super-mario-astar-master/Metacentrum-Scripts/metascript-astar.sh
+++ super-mario-astar-master/Metacentrum-Scripts/metascript-astar.sh
@@ -4,6 +4,7 @@ for st in $(seq 1 1 10)
 do
     for ttf in $(seq 0.2 0.2 2)
     do 
+        echo $st $ttf
         qsub -v searchSteps=$st,timeToFinishWeight=$ttf script-astar2.sh 
     done
 done
