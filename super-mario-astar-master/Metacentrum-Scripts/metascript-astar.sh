80f387f6e546b7e2760e692752ab80befdb4a06c
hgandras <hgandras@gmail.com>
2024-01-13 16:46:42 +0100

    syntax corrected

--- super-mario-astar-master/Metacentrum-Scripts/metascript-astar.sh
+++ super-mario-astar-master/Metacentrum-Scripts/metascript-astar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for st in {1,10,1}
+for st in {1..10..1}
 do
-    for ttf in {0.2,2.0,0.2}
+    for ttf in {0.2..2.0..0.2}
     do 
         qsub -v searchSteps=$st,timeToFinishWeight=$ttf script-astar2.sh 
     done
