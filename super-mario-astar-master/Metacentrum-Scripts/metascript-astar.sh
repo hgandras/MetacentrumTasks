ccef0ab7d63f718b3a9425a79ee3eb6c91ba0446
hgandras <hgandras@gmail.com>
2024-01-13 16:38:55 +0100

    for loop added to script

--- super-mario-astar-master/Metacentrum-Scripts/metascript-astar.sh
+++ super-mario-astar-master/Metacentrum-Scripts/metascript-astar.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-qsub -v searchSteps=3,timeToFinishWeight=1.0 script-astar2.sh 
+for st in {1,10,1}
+do
+    for ttf in {0.2,2.0,0.2}
+    do 
+        qsub -v searchSteps=$st,timeToFinishWeight=$ttf script-astar2.sh 
+    done
+done
